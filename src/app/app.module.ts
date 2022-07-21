22c58d04a26ad89e6975a49fd10ff80e673e1379
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2022-07-21 08:21:57 -0300

    using pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
 import { FormsModule } from '@angular/forms';
 import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 import { HttpClientModule } from '@angular/common/http';
