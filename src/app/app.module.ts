23c6b80ff205d0b8cae0dc7d528473c87fa86e48
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2022-07-21 07:23:32 -0300

    created route event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { APP_BASE_HREF } from '@angular/common';
     [RouterModule.forRoot(rootRouterConfig)],
   ],
   providers: [
-    {provide: APP_BASE_HREF, useValue: '/SmartStore'}
+    {provide: APP_BASE_HREF, useValue: '/'}
   ],
   bootstrap: [AppComponent]
 })
